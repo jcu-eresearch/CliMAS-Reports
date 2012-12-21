921e60111524cb50d2a79e7247b67749730625b4
Daniel Baird <danielbaird@gmail.com>
2012-12-21 15:42:20 +1000

    zipping

--- webapplication/process_data.rb
+++ webapplication/process_data.rb
@@ -6,7 +6,7 @@ require './settings.rb'
 
 region_dirs = Dir.new(Settings::DataFilePrefix + 'regions')
 
-abort "Didn't find #{region_dirs}.. is your settings.rb file correct?" unless File.directory? region_dirs
+abort "Didn't find #{region_dirs}.. is your settings.rb file correct?" unless File.directory? region_dirs.to_s
 
 region_dirs.entries.each do |dir|
 	dirpath = File.join region_dirs.to_s, dir.to_s
